0e53fb8c498f894271132f16b45e592380509ec4
arjahid <jhjahidhasanar@gmail.com>
2025-05-12 22:24:36 +0600

    added getting user api

--- index.js
+++ index.js
@@ -35,9 +35,17 @@ async function run() {
 
 
     // users collection
+    app.get('/users',async(req,res)=>{
+      const result=await userCollection.find().toArray();
+      res.send(result)
+    })
     app.post('/users',async(req,res)=>{
       const user=req.body;
       const query={email:user.email}
+      const existingUser=await userCollection.findOne(query);
+      if(existingUser){
+        return res.send({message:'user already exists'})
+      }
       const result=await userCollection.insertOne(user);
       res.send(result)
     })  
