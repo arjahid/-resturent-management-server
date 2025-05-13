2ab5906356e403746632cea94b2f00c559c43798
arjahid <jhjahidhasanar@gmail.com>
2025-05-13 23:56:14 +0600

    verifying jwt token

--- index.js
+++ index.js
@@ -42,8 +42,27 @@ async function run() {
       res.send({token})
     })
 
+    // verify jwt middleware
+    const verifyToken=(req,res,next)=>{
+      console.log('inside',req.headers)
+     if(!req.headers.authorization){
+      return res.status(401).send({message:'unauthorized access'})
+     }
+      const token=req.headers.authorization.split(' ')[1];
+      jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+        if(err){
+          return res.status(401).send({message:'unauthorized access'})
+        }
+        req.decoded=decoded;
+        next();
+      })
+     
+      // next()
+    }
+
     // users collection
-    app.get('/users', async (req, res) => {
+    app.get('/users',verifyToken, async (req, res) => {
+     
       const result = await userCollection.find().toArray();
       res.send(result)
     })
