2b9c8f03090f896230f3f103f09f7c518eeebdaa
arjahid <jhjahidhasanar@gmail.com>
2025-05-21 19:54:16 +0600

    add cors origin

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ const port = process.env.PORT || 3000;
 
 
 app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
 app.use(express.json());
 
 
@@ -31,7 +35,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     const userCollection = client.db('resturentDb').collection('users');
     const menuCollection = client.db('resturentDb').collection('menu');
     const reviewCollection = client.db('resturentDb').collection('reviews');
@@ -358,8 +362,8 @@ async function run() {
 
 
 
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
