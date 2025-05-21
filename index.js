daa1ed5d2079ca1da97f870fa804aaf7dbb63258
arjahid <jhjahidhasanar@gmail.com>
2025-05-21 21:08:54 +0600

    fix token

--- index.js
+++ index.js
@@ -44,28 +44,11 @@ async function run() {
 
 
     // jwt token
-    // app.post('/jwt',async (req, res) => {
-    //   const {email}=req.body;
-    //   const user = req.body;
-    //   const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
-    //   res.send({ token })
-    // })
-    // Express backend
-app.post("/jwt", async (req, res) => {
-  const { email } = req.body;
-  const user = await userCollection.findOne({ email });
-
-  if (!user) {
-    await userCollection.insertOne({ email, role: "user" }); // default role
-  }
-
-  const token = jwt.sign({ email }, process.env.ACCESS_TOKEN_SECRET, {
-    expiresIn: "1d",
-  });
-
-  res.send({ token });
-});
-
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '5h' });
+      res.send({ token });
+    })
 
     // verify jwt middleware
     const verifyToken = (req, res, next) => {
